379d0a146ff2e6a80a415cadc8aaf11e1776a7a5
Isaac Parr <ikeparr@byu.edu>
2025-01-18 13:28:39 -0700

    Initial creation of specific piece calculators

---
+++ shared/src/main/java/chess/PiecesMoveCalculator/KingMoveCalculator.java
@@ -0,0 +1,6 @@
+package chess.PiecesMoveCalculator;
+
+public class KingMoveCalculator {
+    //Check Up, down, dl, dr, ul, ur, left, and right
+    //then check if valid squares are empty or has other team
+}
---
+++ shared/src/main/java/chess/PiecesMoveCalculator/RookMoveCalculator.java
@@ -0,0 +1,4 @@
+package chess.PiecesMoveCalculator;
+
+public class RookMoveCalculator {
+}
