d878ff8220708b42f1545562e2724aee21235e88
Isaac Parr <ikeparr@byu.edu>
2025-01-18 16:21:22 -0700

    Implemented the KingMoveCalculator class

--- shared/src/main/java/chess/PiecesMoveCalculator/KingMoveCalculator.java
+++ shared/src/main/java/chess/PiecesMoveCalculator/KingMoveCalculator.java
@@ -1,6 +1,101 @@
 package chess.PiecesMoveCalculator;
 
-public class KingMoveCalculator {
-    //Check Up, down, dl, dr, ul, ur, left, and right
-    //then check if valid squares are empty or has other team
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.ArrayList;
+
+public class KingMoveCalculator implements PieceMoveCalculator {
+    public ArrayList<ChessMove> possibleMoves(ChessBoard chessBoard, ChessPosition currentPosition, ChessGame.TeamColor teamColor) {
+        ArrayList<ChessMove> moves = new ArrayList<>();
+
+        int startRow = currentPosition.getRow();
+        int startColumn = currentPosition.getColumn();
+
+        //Up
+            if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn)) {
+                if (PieceMoveCalculator.isValidSquare(chessBoard, startRow + 1, startColumn, teamColor)) {
+                    ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
+                }
+            }
+
+
+        //Down
+
+            if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn)) {
+                if (PieceMoveCalculator.isValidSquare(chessBoard, startRow - 1, startColumn, teamColor)) {
+                    ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
+                }
+            }
+
+
+        //Left
+
+            if (PieceMoveCalculator.moveInBounds(startRow, startColumn - 1)) {
+                if (PieceMoveCalculator.isValidSquare(chessBoard, startRow, startColumn - 1, teamColor)) {
+                    ChessPosition newPosition = new ChessPosition(startRow, startColumn - 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
+                }
+            }
+
+
+        //Right
+
+            if (PieceMoveCalculator.moveInBounds(startRow, startColumn + 1)) {
+                if (PieceMoveCalculator.isValidSquare(chessBoard, startRow, startColumn + 1, teamColor)) {
+                    ChessPosition newPosition = new ChessPosition(startRow, startColumn + 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
+                }
+            }
+
+
+        //UpRIGHT
+
+            if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn + 1)) {
+                if (PieceMoveCalculator.isValidSquare(chessBoard, startRow + 1, startColumn + 1, teamColor)) {
+                    ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn + 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
+                }
+            }
+
+
+        //UpLEFT
+
+            if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn - 1)) {
+                if (PieceMoveCalculator.isValidSquare(chessBoard, startRow + 1, startColumn - 1, teamColor)) {
+                    ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn - 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
+
+                }
+            }
+
+
+        //DownLEFT
+
+            if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn - 1)) {
+                if (PieceMoveCalculator.isValidSquare(chessBoard, startRow - 1, startColumn - 1, teamColor)) {
+                    ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn - 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
+
+                }
+            }
+
+
+        //DownRIGHT
+
+            if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn + 1)) {
+                if (PieceMoveCalculator.isValidSquare(chessBoard, startRow - 1, startColumn + 1, teamColor)) {
+                    ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn + 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
+
+                }
+            }
+
+
+        return moves;
+    }
 }
