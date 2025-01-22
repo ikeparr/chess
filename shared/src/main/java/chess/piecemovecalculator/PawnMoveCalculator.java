bfd43177ed768c1c685bea190d18eb0f71dc01df
Isaac Parr <ikeparr@byu.edu>
2025-01-21 21:42:13 -0700

    adjusted the nested "if" blocks

--- shared/src/main/java/chess/piecemovecalculator/PawnMoveCalculator.java
+++ shared/src/main/java/chess/piecemovecalculator/PawnMoveCalculator.java
@@ -8,71 +8,67 @@ public class PawnMoveCalculator implements PieceMoveCalculator {
         ArrayList<ChessMove> moves = new ArrayList<>();
         int startRow = currentPosition.getRow();
         int startColumn = currentPosition.getColumn();
+
         if (teamColor == ChessGame.TeamColor.WHITE) {
 
             //CHECK IF FIRST MOVE
             if (startRow == 2) {
-                if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn)) {
-                    if (PieceMoveCalculator.isFreeSquare(chessBoard, startRow + 1, startColumn)) {
-                        ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn);
-                        moves.add(new ChessMove(currentPosition, newPosition, null));
-                        if (PieceMoveCalculator.moveInBounds(startRow + 2, startColumn)) {
-                            if (PieceMoveCalculator.isFreeSquare(chessBoard, startRow + 2, startColumn)) {
-                                ChessPosition doubleNewPosition = new ChessPosition(startRow + 2, startColumn);
-                                moves.add(new ChessMove(currentPosition, doubleNewPosition, null));
-                            }
-                        }
+                if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn)
+                        && PieceMoveCalculator.isFreeSquare(chessBoard, startRow + 1, startColumn)) {
+                    ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
+                    if (PieceMoveCalculator.moveInBounds(startRow + 2, startColumn)
+                            && PieceMoveCalculator.isFreeSquare(chessBoard, startRow + 2, startColumn)) {
+                        ChessPosition doubleNewPosition = new ChessPosition(startRow + 2, startColumn);
+                        moves.add(new ChessMove(currentPosition, doubleNewPosition, null));
                     }
                 }
             }
 
             //ALL OTHER STARTING ROWS
             else {
-                if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn)) {
-                    if (PieceMoveCalculator.isFreeSquare(chessBoard, startRow + 1, startColumn)) {
-                        if (startRow + 1 == 8) {
-                            ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn);
-                            moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.QUEEN));
-                            moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.KNIGHT));
-                            moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.ROOK));
-                            moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.BISHOP));
-                        } else {
-                            ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn);
-                            moves.add(new ChessMove(currentPosition, newPosition, null));
-                        }
+                if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn)
+                        && PieceMoveCalculator.isFreeSquare(chessBoard, startRow + 1, startColumn)) {
+                    if (startRow + 1 == 8) {
+                        ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn);
+                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.QUEEN));
+                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.KNIGHT));
+                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.ROOK));
+                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.BISHOP));
+                    } else {
+                        ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn);
+                        moves.add(new ChessMove(currentPosition, newPosition, null));
                     }
                 }
             }
 
             //LEFT-UP DIAGONAL ATTACK
-            if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn - 1)) {
-                if (PieceMoveCalculator.isOpponentSquare(chessBoard, startRow + 1, startColumn - 1, teamColor)) {
-                    if (startRow + 1 == 8) {
-                        ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn - 1);
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.QUEEN));
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.KNIGHT));
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.ROOK));
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.BISHOP));
-                    } else {
-                        ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn - 1);
-                        moves.add(new ChessMove(currentPosition, newPosition, null));
-                    }
+            if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn - 1)
+                    && PieceMoveCalculator.isOpponentSquare(chessBoard, startRow + 1, startColumn - 1, teamColor)) {
+                if (startRow + 1 == 8) {
+                    ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn - 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.QUEEN));
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.KNIGHT));
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.ROOK));
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.BISHOP));
+                } else {
+                    ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn - 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
                 }
             }
 
             //RIGHT-UP DIAGONAL ATTACK
-            if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn + 1)) {
-                if (PieceMoveCalculator.isOpponentSquare(chessBoard, startRow + 1, startColumn + 1, teamColor)) {
-                    if (startRow + 1 == 8) {
-                        ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn + 1);
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.QUEEN));
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.KNIGHT));
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.ROOK));
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.BISHOP));
-                    } else {
-                        ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn + 1);
-                        moves.add(new ChessMove(currentPosition, newPosition, null));
-                    }
+            if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn + 1)
+                    && PieceMoveCalculator.isOpponentSquare(chessBoard, startRow + 1, startColumn + 1, teamColor)) {
+                if (startRow + 1 == 8) {
+                    ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn + 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.QUEEN));
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.KNIGHT));
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.ROOK));
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.BISHOP));
+                } else {
+                    ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn + 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
                 }
             }
         }
@@ -81,67 +77,62 @@ public class PawnMoveCalculator implements PieceMoveCalculator {
 
             //CHECK IF FIRST MOVE
             if (startRow == 7) {
-                if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn)) {
-                    if (PieceMoveCalculator.isFreeSquare(chessBoard, startRow - 1, startColumn)) {
-                        ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn);
-                        moves.add(new ChessMove(currentPosition, newPosition, null));
-                        if (PieceMoveCalculator.moveInBounds(startRow - 2, startColumn)) {
-                            if (PieceMoveCalculator.isFreeSquare(chessBoard, startRow - 2, startColumn)) {
-                                ChessPosition doubleNewPosition = new ChessPosition(startRow - 2, startColumn);
-                                moves.add(new ChessMove(currentPosition, doubleNewPosition, null));
-                            }
-                        }
+                if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn)
+                        && PieceMoveCalculator.isFreeSquare(chessBoard, startRow - 1, startColumn)) {
+                    ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
+                    if (PieceMoveCalculator.moveInBounds(startRow - 2, startColumn)
+                            && PieceMoveCalculator.isFreeSquare(chessBoard, startRow - 2, startColumn)) {
+                        ChessPosition doubleNewPosition = new ChessPosition(startRow - 2, startColumn);
+                        moves.add(new ChessMove(currentPosition, doubleNewPosition, null));
                     }
                 }
             }
 
             //ALL OTHER STARTING ROWS
             else {
-                if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn)) {
-                    if (PieceMoveCalculator.isFreeSquare(chessBoard, startRow - 1, startColumn)) {
-                        if (startRow - 1 == 1) {
-                            ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn);
-                            moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.QUEEN));
-                            moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.KNIGHT));
-                            moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.ROOK));
-                            moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.BISHOP));
-                        } else {
-                            ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn);
-                            moves.add(new ChessMove(currentPosition, newPosition, null));
-                        }
+                if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn)
+                        && PieceMoveCalculator.isFreeSquare(chessBoard, startRow - 1, startColumn)) {
+                    if (startRow - 1 == 1) {
+                        ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn);
+                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.QUEEN));
+                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.KNIGHT));
+                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.ROOK));
+                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.BISHOP));
+                    } else {
+                        ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn);
+                        moves.add(new ChessMove(currentPosition, newPosition, null));
                     }
                 }
             }
 
             //LEFT-DOWN DIAGONAL ATTACK
-            if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn - 1)) {
-                if (PieceMoveCalculator.isOpponentSquare(chessBoard, startRow - 1, startColumn - 1, teamColor)) {
-                    if (startRow - 1 == 1) {
-                        ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn - 1);
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.QUEEN));
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.KNIGHT));
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.ROOK));
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.BISHOP));
-                    } else {
-                        ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn - 1);
-                        moves.add(new ChessMove(currentPosition, newPosition, null));
-                    }
+            if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn - 1)
+                    && PieceMoveCalculator.isOpponentSquare(chessBoard, startRow - 1, startColumn - 1, teamColor)) {
+                if (startRow - 1 == 1) {
+                    ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn - 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.QUEEN));
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.KNIGHT));
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.ROOK));
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.BISHOP));
+                } else {
+                    ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn - 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
                 }
             }
 
             //RIGHT DOWN DIAGONAL ATTACK
-            if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn + 1)) {
-                if (PieceMoveCalculator.isOpponentSquare(chessBoard, startRow - 1, startColumn + 1, teamColor)) {
-                    if (startRow - 1 == 1) {
-                        ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn + 1);
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.QUEEN));
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.KNIGHT));
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.ROOK));
-                        moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.BISHOP));
-                    } else {
-                        ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn + 1);
-                        moves.add(new ChessMove(currentPosition, newPosition, null));
-                    }
+            if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn + 1)
+                    && PieceMoveCalculator.isOpponentSquare(chessBoard, startRow - 1, startColumn + 1, teamColor)) {
+                if (startRow - 1 == 1) {
+                    ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn + 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.QUEEN));
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.KNIGHT));
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.ROOK));
+                    moves.add(new ChessMove(currentPosition, newPosition, ChessPiece.PieceType.BISHOP));
+                } else {
+                    ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn + 1);
+                    moves.add(new ChessMove(currentPosition, newPosition, null));
                 }
             }
         }
