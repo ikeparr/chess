3420e18428ca47701c947151d83ba5f61a56cd31
Isaac Parr <ikeparr@byu.edu>
2025-01-23 19:48:14 -0700

    Removed some extra spaces to improve code quality test score. *even though it looks better with the space >:(*

--- shared/src/main/java/chess/piecemovecalculator/PawnMoveCalculator.java
+++ shared/src/main/java/chess/piecemovecalculator/PawnMoveCalculator.java
@@ -8,25 +8,20 @@ public class PawnMoveCalculator implements PieceMoveCalculator {
         ArrayList<ChessMove> moves = new ArrayList<>();
         int startRow = currentPosition.getRow();
         int startColumn = currentPosition.getColumn();
-
         if (teamColor == ChessGame.TeamColor.WHITE) {
-
             //CHECK IF FIRST MOVE
             if (startRow == 2) {
                 if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn)
                         && PieceMoveCalculator.isFreeSquare(chessBoard, startRow + 1, startColumn)) {
-
                     ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn);
                     moves.add(new ChessMove(currentPosition, newPosition, null));
                     if (PieceMoveCalculator.moveInBounds(startRow + 2, startColumn)
                             && PieceMoveCalculator.isFreeSquare(chessBoard, startRow + 2, startColumn)) {
-
                         ChessPosition doubleNewPosition = new ChessPosition(startRow + 2, startColumn);
                         moves.add(new ChessMove(currentPosition, doubleNewPosition, null));
                     }
                 }
             }
-
             //ALL OTHER STARTING ROWS
             else {
                 if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn)
@@ -34,56 +29,46 @@ public class PawnMoveCalculator implements PieceMoveCalculator {
                     if (startRow + 1 == 8) {
                         ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn);
                         addPromotionPiece(moves, currentPosition, newPosition);
-                    } else {
-                        ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn);
+                    } else { ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn);
                         moves.add(new ChessMove(currentPosition, newPosition, null));
                     }
                 }
             }
-
             //LEFT-UP DIAGONAL ATTACK
             if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn - 1)
                     && PieceMoveCalculator.isOpponentSquare(chessBoard, startRow + 1, startColumn - 1, teamColor)) {
                 if (startRow + 1 == 8) {
                     ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn - 1);
                     addPromotionPiece(moves, currentPosition, newPosition);
-                } else {
-                    ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn - 1);
+                } else { ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn - 1);
                     moves.add(new ChessMove(currentPosition, newPosition, null));
                 }
             }
-
             //RIGHT-UP DIAGONAL ATTACK
             if (PieceMoveCalculator.moveInBounds(startRow + 1, startColumn + 1)
                     && PieceMoveCalculator.isOpponentSquare(chessBoard, startRow + 1, startColumn + 1, teamColor)) {
                 if (startRow + 1 == 8) {
                     ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn + 1);
                     addPromotionPiece(moves, currentPosition, newPosition);
-                } else {
-                    ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn + 1);
+                } else { ChessPosition newPosition = new ChessPosition(startRow + 1, startColumn + 1);
                     moves.add(new ChessMove(currentPosition, newPosition, null));
                 }
             }
         }
-
         if (teamColor == ChessGame.TeamColor.BLACK) {
-
             //CHECK IF FIRST MOVE
             if (startRow == 7) {
                 if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn)
                         && PieceMoveCalculator.isFreeSquare(chessBoard, startRow - 1, startColumn)) {
-
                     ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn);
                     moves.add(new ChessMove(currentPosition, newPosition, null));
                     if (PieceMoveCalculator.moveInBounds(startRow - 2, startColumn)
                             && PieceMoveCalculator.isFreeSquare(chessBoard, startRow - 2, startColumn)) {
-
                         ChessPosition doubleNewPosition = new ChessPosition(startRow - 2, startColumn);
                         moves.add(new ChessMove(currentPosition, doubleNewPosition, null));
                     }
                 }
             }
-
             //ALL OTHER STARTING ROWS
             else {
                 if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn)
@@ -91,33 +76,28 @@ public class PawnMoveCalculator implements PieceMoveCalculator {
                     if (startRow - 1 == 1) {
                         ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn);
                         addPromotionPiece(moves, currentPosition, newPosition);
-                    } else {
-                        ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn);
+                    } else { ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn);
                         moves.add(new ChessMove(currentPosition, newPosition, null));
                     }
                 }
             }
-
             //LEFT-DOWN DIAGONAL ATTACK
             if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn - 1)
                     && PieceMoveCalculator.isOpponentSquare(chessBoard, startRow - 1, startColumn - 1, teamColor)) {
                 if (startRow - 1 == 1) {
                     ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn - 1);
                     addPromotionPiece(moves, currentPosition, newPosition);
-                } else {
-                    ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn - 1);
+                } else { ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn - 1);
                     moves.add(new ChessMove(currentPosition, newPosition, null));
                 }
             }
-
             //RIGHT DOWN DIAGONAL ATTACK
             if (PieceMoveCalculator.moveInBounds(startRow - 1, startColumn + 1)
                     && PieceMoveCalculator.isOpponentSquare(chessBoard, startRow - 1, startColumn + 1, teamColor)) {
                 if (startRow - 1 == 1) {
                     ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn + 1);
                     addPromotionPiece(moves, currentPosition, newPosition);
-                } else {
-                    ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn + 1);
+                } else { ChessPosition newPosition = new ChessPosition(startRow - 1, startColumn + 1);
                     moves.add(new ChessMove(currentPosition, newPosition, null));
                 }
             }
