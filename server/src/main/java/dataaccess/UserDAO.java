e1184f9da9b291c39ae63fd9074412f4d8387cff
Isaac Parr <ikeparr@byu.edu>
2025-02-25 13:39:37 -0700

    Implementation of the UserDAO interface, may adjust method of storage for getUser (pending better way)

--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -2,4 +2,9 @@ package dataaccess;
 import model.UserData;
 
 public interface UserDAO {
+
+    void clear() throws DataAccessException;
+    void createUser(UserData user) throws DataAccessException;
+    /// ASSESS WHETHER THERE IS A MORE EFFICIENT WAY
+    UserData getUser(String username) throws DataAccessException;
 }
