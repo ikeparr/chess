118d7ce1d6cd2cc1ea0c6a500ba2d08e5a5dc2c6
Isaac Parr <ikeparr@byu.edu>
2025-02-25 13:38:30 -0700

    Implementation of the GameDAO interface, may adjust method of storage for getGame

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,5 +1,14 @@
 package dataaccess;
 import model.GameData;
 
+import java.util.Collection;
+
 public interface GameDAO {
+
+    void createGame(GameData game) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    /// NOTE: Collections are similar to maps i think
+    Collection<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+    void clear() throws DataAccessException;
 }
