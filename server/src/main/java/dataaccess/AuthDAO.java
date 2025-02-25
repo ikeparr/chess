f831b5a6447f4adb770c688a9c037b76753d5d58
Isaac Parr <ikeparr@byu.edu>
2025-02-25 13:39:12 -0700

    Implementation of the AuthDAO interface, may adjust method of storage for getAuth (pending better way)

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -2,4 +2,11 @@ package dataaccess;
 import model.AuthData;
 
 public interface AuthDAO {
+
+    void clear() throws DataAccessException;
+    void createAuth(AuthData auth) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+    /// just like userdao, check if better way ///
+    AuthData getAuth(String authToken) throws DataAccessException;
+
 }
