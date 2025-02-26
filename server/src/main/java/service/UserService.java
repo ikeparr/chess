871c1aa0f0603e31b015426b6850ed313b6a32b7
Isaac Parr <ikeparr@byu.edu>
2025-02-26 12:14:11 -0700

    Initialized userDAO and authDAO, implemented clear method

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,4 +1,25 @@
 package service;
 
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+
+import java.util.UUID;
+
 public class UserService {
+
+    /// NOTE: AuthService merged into UserService, they're both used together a lot ///
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+
+    public UserService(UserDAO userDAO, AuthDAO authDAO){
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+
+    }
+
+    public void clear() throws DataAccessException{
+        userDAO.clear();
+        authDAO.clear();
+    }
 }
