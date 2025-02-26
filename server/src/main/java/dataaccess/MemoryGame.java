4bdfaeac73372b9190b370c9c86b72b278d296de
Isaac Parr <ikeparr@byu.edu>
2025-02-26 12:11:47 -0700

    creation and implementation the MeomoryGame DAO class for testing

---
+++ server/src/main/java/dataaccess/MemoryGame.java
@@ -0,0 +1,32 @@
+package dataaccess;
+
+import model.GameData;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryGame implements GameDAO {
+    private final Map<Integer, GameData> games = new HashMap<>();
+
+    public void clear() throws DataAccessException{
+        games.clear();
+    }
+
+
+    public void updateGame(GameData game) throws DataAccessException {
+
+    }
+
+    public void createGame(GameData game) throws DataAccessException{
+        games.put(game.gameID(), game);
+    }
+
+    public GameData getGame(int gameID) throws DataAccessException {
+        return games.get(gameID);
+    }
+
+    public Collection<GameData> listGames() throws DataAccessException{
+        return games.values();
+    }
+}
