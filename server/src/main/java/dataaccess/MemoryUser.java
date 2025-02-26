a9ce773a66e8f1e4c424b38a4e3d23f4571ab056
Isaac Parr <ikeparr@byu.edu>
2025-02-26 12:11:07 -0700

    creation of the MemeoryUser DAO class.

---
+++ server/src/main/java/dataaccess/MemoryUser.java
@@ -0,0 +1,26 @@
+package dataaccess;
+
+import model.UserData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryUser implements UserDAO {
+    private final Map<String, UserData> users = new HashMap<>();
+
+
+    public void clear() throws DataAccessException {
+        users.clear();
+    }
+
+    public void createUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("Username already taken :(");
+        }
+        users.put(user.username(), user);
+    }
+
+    public UserData getUser(String username) throws DataAccessException{
+        return users.get(username);
+    }
+}
