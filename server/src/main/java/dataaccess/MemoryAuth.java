f2ab6b534881858d63909b1cf94753ff8c9bd828
Isaac Parr <ikeparr@byu.edu>
2025-02-26 12:12:00 -0700

    creation and implementation the MeomoryAuth DAO class for testing

---
+++ server/src/main/java/dataaccess/MemoryAuth.java
@@ -0,0 +1,28 @@
+package dataaccess;
+import model.AuthData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryAuth implements AuthDAO {
+    private final Map<String, AuthData> authTokens = new HashMap<>();
+
+
+    public void clear() throws DataAccessException {
+        authTokens.clear();
+    }
+
+    public void createAuth(AuthData auth) throws DataAccessException {
+        authTokens.put(auth.authToken(), auth);
+    }
+
+    public AuthData getAuth(String authToken) throws DataAccessException{
+        return authTokens.get(authToken);
+    }
+
+    public void deleteAuth(String authToken) throws DataAccessException {
+        authTokens.remove(authToken);
+
+    }
+}
