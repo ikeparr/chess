3c85acafdcc819f9616dfe959a19531baa697010
Isaac Parr <ikeparr@byu.edu>
2025-02-26 12:18:08 -0700

    added endpoints to server, initialize authDAO, gameDAO, userDAO

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,13 @@
 package server;
 
+import handlers.*;
 import spark.*;
+import dataaccess.*;
 
 public class Server {
+    private final AuthDAO authDAO = new MemoryAuth();
+    private final GameDAO gameDAO = new MemoryGame();
+    private final UserDAO userDAO = new MemoryUser();
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -13,6 +18,12 @@ public class Server {
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
+        Spark.delete("/db", new ClearHandler(authDAO, gameDAO, userDAO));
+        /// YET TO BE IMPLEMENTED BELOW ///
+        //Spark.delete("/session", new LogoutHandler(authDAO)::handle);
+        //Spark.get("/game", new ListGamesHandler(authDAO)::handle);
+        //Spark.post("/game", new CreateGameHandler( ??? )::handle);
+        //Spark.put("/game", new JoinGameHandler( ??? )::handle
 
         Spark.awaitInitialization();
         return Spark.port();
