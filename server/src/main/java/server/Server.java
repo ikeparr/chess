e0b4cef5c963a00625aa07d653bf6ec5d030f758
Isaac Parr <ikeparr@byu.edu>
2025-03-03 12:04:46 -0700

    implemented JoinGame endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -11,24 +11,16 @@ public class Server {
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
-
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
         Spark.delete("/db", new ClearHandler(authDAO, gameDAO, userDAO));
         Spark.post("/user", new RegisterHandler(userDAO, authDAO));
         Spark.post("/session", new LoginHandler(userDAO, authDAO));
         Spark.delete("/session", new LogoutHandler(userDAO, authDAO));
         Spark.post("/game", new CreateGameHandler(userDAO, authDAO, gameDAO));
         Spark.get("/game", new ListGamesHandler(userDAO, authDAO, gameDAO));
-        /// YET TO BE IMPLEMENTED BELOW ///
-        //Spark.delete("/session", new LogoutHandler(authDAO)::handle);
-        //Spark.get("/game", new ListGamesHandler(authDAO)::handle);
-        //Spark.post("/game", new CreateGameHandler( ??? )::handle);
-        //Spark.put("/game", new JoinGameHandler( ??? )::handle
+        Spark.put("/game", new JoinGameHandler(userDAO, authDAO, gameDAO));
 
         //This line initializes the server and can be removed once you have a functioning endpoint
         // Spark.init();
