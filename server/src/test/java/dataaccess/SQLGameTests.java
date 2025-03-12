2a4416065216da56d6d903590fb8dc281adfbfa9
Isaac Parr <ikeparr@byu.edu>
2025-03-11 22:56:07 -0600

    before each, create game successful, and clear successful all implemented

--- server/src/test/java/dataaccess/SQLGameTests.java
+++ server/src/test/java/dataaccess/SQLGameTests.java
@@ -11,5 +11,45 @@ public class SQLGameTests {
     private static SQLGame sqlGame;
     private static SQLUser sqlUser;
 
+    @BeforeEach
+    void setUp() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        sqlUser = new SQLUser();
+        sqlGame = new SQLGame();
+        sqlGame.clear();
+        sqlUser.clear();
+    }
 
+
+    /// GAME CREATION TESTS ///
+    @Test
+    void createGameSuccessful() throws DataAccessException {
+        GameData game = new GameData(1234, null, null, "testGameName", new ChessGame());
+        boolean successfulTest = true;
+        try {
+            sqlGame.createGame(game);
+        }
+        catch (DataAccessException error) {
+            successfulTest = false;
+        }
+        assert successfulTest;
+        // Verify game was added
+        GameData result = sqlGame.getGame(1234);
+        assert result != null;
+    }
+    @Test
+    void clearSuccessful() throws DataAccessException {
+        GameData game = new GameData(999, null, null, "clearGame", new ChessGame());
+        sqlGame.createGame(game);
+        boolean successfulTest = true;
+        try {
+            sqlGame.clear();
+        }
+        catch (DataAccessException error) {
+            successfulTest = false;
+        }
+        assert successfulTest;
+        Collection<GameData> results = sqlGame.listGames();
+        assert results.isEmpty();
+    }
 }
