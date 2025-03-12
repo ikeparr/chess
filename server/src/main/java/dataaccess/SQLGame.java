ac37ab2a1a37132c277b2f8e2402b6238923c807
Isaac Parr <ikeparr@byu.edu>
2025-03-11 22:47:12 -0600

    initial commit

---
+++ server/src/main/java/dataaccess/SQLGame.java
@@ -0,0 +1,15 @@
+package dataaccess;
+
+import chess.ChessGame;
+import com.google.gson.Gson;
+import model.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.sql.*;
+import static java.sql.Types.NULL;
+import java.sql.Types;
+
+public class SQLGame implements GameDAO {
+
+
+}
