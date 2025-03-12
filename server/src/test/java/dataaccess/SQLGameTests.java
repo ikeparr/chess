ef222072b3bd57d1ceef14fe4206277a3ee2a2a1
Isaac Parr <ikeparr@byu.edu>
2025-03-11 22:54:37 -0600

    initial commit

---
+++ server/src/test/java/dataaccess/SQLGameTests.java
@@ -0,0 +1,15 @@
+package dataaccess;
+
+import chess.ChessGame;
+import model.GameData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.Collection;
+
+public class SQLGameTests {
+    private static SQLGame sqlGame;
+    private static SQLUser sqlUser;
+
+
+}
