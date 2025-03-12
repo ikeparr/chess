74e0df5416cdbe5ba9f838d6a989194cef7b9710
Isaac Parr <ikeparr@byu.edu>
2025-03-12 17:08:53 -0600

    added updateGame back to the GameDAO, forgot I had removed it

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -7,5 +7,6 @@ public interface GameDAO {
     void createGame(GameData game) throws DataAccessException;
     GameData getGame(int gameID) throws DataAccessException;
     Collection<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
     void clear() throws DataAccessException;
 }
