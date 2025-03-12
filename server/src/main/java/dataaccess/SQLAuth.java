ff5f13db8687667ef1a4ea15c89bdf534e88e1c3
Isaac Parr <ikeparr@byu.edu>
2025-03-12 17:47:45 -0600

    sqlauth and clear method implemented

--- server/src/main/java/dataaccess/SQLAuth.java
+++ server/src/main/java/dataaccess/SQLAuth.java
@@ -6,4 +6,24 @@ import static java.sql.Types.NULL;
 
 public class SQLAuth implements AuthDAO {
 
+    public SQLAuth() {
+        try {
+            configureAuthDatabase();
+        }
+        catch (DataAccessException error) {
+            throw new RuntimeException(error);
+        }
+    }
+
+
+
+    public void clear() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "DELETE FROM auths";
+            executeUpdate(statement);
+        }
+        catch (SQLException ex) {
+            throw new DataAccessException("Error: couldnt clear auth table: " + ex.getMessage());
+        }
+    }
 }
