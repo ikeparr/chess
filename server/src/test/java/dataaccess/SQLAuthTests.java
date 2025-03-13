eaceebe7be4b520ead69252bf7dfdf3d7a11d261
Isaac Parr <ikeparr@byu.edu>
2025-03-12 18:09:05 -0600

    setup, pos and neg createAuth tests added

--- server/src/test/java/dataaccess/SQLAuthTests.java
+++ server/src/test/java/dataaccess/SQLAuthTests.java
@@ -11,4 +11,41 @@ public class SQLAuthTests {
     private SQLAuth sqlAuth;
     private SQLUser sqlUser;
 
+    @BeforeEach
+    void setUp() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        sqlUser = new SQLUser();
+        sqlAuth = new SQLAuth();
+        sqlAuth.clear();
+        sqlUser.clear();
+    }
+
+    @Test
+    void createAuthSuccessful() throws DataAccessException {
+        sqlUser.createUser(new UserData("testAuthUserBOBBY", "password", "auth@gmail.com"));
+        String authToken = UUID.randomUUID().toString();
+        AuthData input = new AuthData(authToken, "testAuthUserBOBBY");
+        boolean successfulTest = true;
+        try {
+            sqlAuth.createAuth(input);
+        }
+        catch (DataAccessException error) {
+            successfulTest = false;
+        }
+        assert successfulTest;
+    }
+    @Test
+    void createAuthFail() throws DataAccessException {
+        String authToken = UUID.randomUUID().toString();
+        AuthData input = new AuthData(authToken, "fakeUsername");
+        boolean failTest = false;
+        try {
+            sqlAuth.createAuth(input);
+            sqlAuth.createAuth(input);
+        }
+        catch (DataAccessException error) {
+            failTest = true;
+        }
+        assert failTest;
+    }
 }
