1c5849ec0e8d8e6b0fc848ef8e3cf142acd4082b
Isaac Parr <ikeparr@byu.edu>
2025-03-12 18:07:23 -0600

    initial commit

---
+++ server/src/test/java/dataaccess/SQLAuthTests.java
@@ -0,0 +1,14 @@
+package dataaccess;
+
+import model.UserData;
+import model.AuthData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class SQLAuthTests {
+    private SQLAuth sqlAuth;
+    private SQLUser sqlUser;
+
+}
