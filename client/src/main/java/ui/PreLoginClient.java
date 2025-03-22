106e78426c7a844bda9fb0815cffe6ac3dbacb93
Isaac Parr <ikeparr@byu.edu>
2025-03-21 19:50:43 -0600

    added inputs, added preloginclient method, and adjusted eval method from petshop to work for prelogin chess

--- client/src/main/java/ui/PreLoginClient.java
+++ client/src/main/java/ui/PreLoginClient.java
@@ -1,4 +1,39 @@
 package ui;
 
+import java.util.Arrays;
+
+import com.google.gson.Gson;
+import com.sun.nio.sctp.NotificationHandler;
+import model.*;
+import exceptions.ResponseException;
+import ui.ServerFacade;
+
 public class PreLoginClient {
+    private final ServerFacade server;
+    private final int serverURL;
+    public String authToken;
+
+
+
+    public PreLoginClient(int serverURL) {
+        server = new ServerFacade(serverURL);
+        this.serverURL = serverURL;
+        authToken = null;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> registerUser(params);
+                case "login" -> loginUser(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
 }
