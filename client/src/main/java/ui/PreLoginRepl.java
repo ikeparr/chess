009830222f7c6b82733863effbbff799fc19bc3d
Isaac Parr <ikeparr@byu.edu>
2025-03-22 14:22:02 -0600

    adjusted petshop code to work for a preLoginRepl. Switches to post login repl when you log in

--- client/src/main/java/ui/PreLoginRepl.java
+++ client/src/main/java/ui/PreLoginRepl.java
@@ -13,5 +13,39 @@ public class PreLoginRepl {
         this.serverURL = serverUrl;
     }
 
+    public void run() {
+        System.out.println("Welcome to CS240 Chess. Login or register to start.");
+        System.out.print(preLoginClient.help());
 
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = preLoginClient.eval(line);
+                System.out.print(SET_TEXT_COLOR_BLUE + result);
+                //             IF AUTHTOKEN != NULL THEN IN POSTLOGIN REPL
+                if (preLoginClient.authToken != null) {
+                    PostLoginRepl postLoginRepl = new PostLoginRepl(serverURL, preLoginClient.authToken);
+                    postLoginRepl.run();
+                    break;
+                }
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+//    public void notify(Notification notification) {
+//        System.out.println(SET_TEXT_COLOR_RED + notification.message());
+//        printPrompt();
+//    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET_TEXT_COLOR + ">>> " + SET_TEXT_COLOR_GREEN);
+    }
 }
