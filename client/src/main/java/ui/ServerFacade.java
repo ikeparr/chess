17b06596612f78c9aab6ad5ae6a3a2cac995bd8e
Isaac Parr <ikeparr@byu.edu>
2025-03-21 19:53:04 -0600

    changed login to take string username and string password, also changed serverURL to take port instead of whole URL

--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -11,15 +11,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.InputStreamReader;
-import java.util.*;
 import java.net.*;
 
 public class ServerFacade {
 
     private final String serverURL;
 
-    public ServerFacade(String url) {
-        serverURL = url;
+    public ServerFacade(int port) {
+        serverURL = "http://localhost:" + port;
     }
 
     public AuthData registerUser(String username, String password, String email) throws ResponseException {
@@ -28,7 +27,7 @@ public class ServerFacade {
         return this.makeRequest("POST", path, user, AuthData.class, null);
     }
 
-    public AuthData loginUser(UserData user) throws ResponseException {
+    public AuthData loginUser(String username, String password) throws ResponseException {
         var path = "/session";
         return this.makeRequest("POST", path, user, AuthData.class, null);
     }
