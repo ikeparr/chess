26410b530bb6a2ae14e64a5018c9fbfee9daf74e
Isaac Parr <ikeparr@byu.edu>
2025-03-24 14:46:53 -0600

    added server url and prelogin repl to run when main is run

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,12 @@
 import chess.*;
+import ui.*;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+        var serverUrl = 8080;
+
+        new PreLoginRepl(serverUrl).run();
     }
 }
