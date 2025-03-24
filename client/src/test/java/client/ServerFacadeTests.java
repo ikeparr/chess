c63cea1d2cc74fb8690a1fdea08adcb1d0dc4088
Isaac Parr <ikeparr@byu.edu>
2025-03-24 14:45:48 -0600

    added before each:Clear all to test and added regiterUser tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,25 @@
 package client;
 
+import model.*;
+import ui.ServerFacade;
+import exceptions.*;
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        serverFacade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -20,10 +27,26 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void clearDatabase() throws ResponseException {
+        serverFacade.makeRequest("DELETE", "/db", null, null, null);
+    }
 
     @Test
     public void sampleTest() {
         Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void registerUserPositive() throws ResponseException {
+        AuthData authData = serverFacade.registerUser("testUser", "testPassword", "email@email.com");
+        assertEquals("testUser", authData.username());
+    }
+
+    @Test
+    public void registerUserNegative() throws ResponseException {
+        AuthData authData = serverFacade.registerUser("testUser", "testPassword", "email@email.com");
+        assertNotEquals("testUser23", authData.username());
     }
 
 }
