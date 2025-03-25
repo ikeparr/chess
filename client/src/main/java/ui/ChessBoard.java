c0d3c818dca90a4523b6684fdf21808038b14fe0
Isaac Parr <ikeparr@byu.edu>
2025-03-24 21:33:38 -0600

    Based off of tic tac toe board, created initial black pov board and initial white pov board

---
+++ client/src/main/java/ui/ChessBoard.java
@@ -0,0 +1,272 @@
+package ui;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+import static ui.EscapeSequences.*;
+
+public class ChessBoard {
+
+    public static final String EMPTY = " \u2003 ";
+
+    //initial white board set up:
+    private static final String[][] INITIAL_BOARD_WHITE = {
+            {BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK},
+            {BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN},
+            {WHITE_ROOK, WHITE_KNIGHT, WHITE_BISHOP, WHITE_QUEEN, WHITE_KING, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK}
+    };
+
+    //initial black board set up:
+    private static final String[][] INITIAL_BOARD_BLACK = {
+            {WHITE_ROOK, WHITE_KNIGHT, WHITE_BISHOP, WHITE_QUEEN, WHITE_KING, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK},
+            {WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN, WHITE_PAWN},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN},
+            {BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK}
+    };
+
+    // Board dimensions.
+    private static final int BOARD_SIZE_IN_SQUARES = 8;
+    private static final int SQUARE_SIZE_IN_PADDED_CHARS = 1;
+    private static final int LINE_WIDTH_IN_PADDED_CHARS = 0;
+
+    public static void main(String[] args) {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        out.print(ERASE_SCREEN);
+        drawWhiteBoard(out);
+        System.out.println("POV White chess board\n");
+        drawBlackBoard(out);
+        System.out.println("POV Black chess board");
+    }
+
+    /// WHITE BOARD SET UP
+    private static void drawWhiteBoard(PrintStream out) {
+        drawHeadersWhite(out);
+        drawWhiteChessBoard(out);
+        drawFootersWhite(out);
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void drawBlackBoard(PrintStream out) {
+        drawHeadersBlack(out);
+        drawBlackChessBoard(out);
+        drawFootersBlack(out);
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+
+    private static void drawHeadersWhite(PrintStream out) {
+        setBlack(out);
+
+        //offset for row #'s
+        out.print("   ");
+        String[] headers = { " a ", " b ", " c ", " d ", " e ", " f ", " g ", " h " };
+        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+            drawHeaderWhite(out, headers[boardCol]);
+            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+                out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
+            }
+        }
+
+        setBlack(out);
+        out.print("  ");
+        out.println();
+    }
+
+    private static void drawFootersWhite(PrintStream out) {
+        setBlack(out);
+
+        // offset for row #'s
+        out.print("   ");
+        String[] footers = { " a ", " b ", " c ", " d ", " e ", " f ", " g ", " h " };
+        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+            drawHeaderWhite(out, footers[boardCol]);
+            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+                out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
+            }
+        }
+
+        setBlack(out);
+        out.print("  ");
+        out.println();
+    }
+
+    private static void drawHeaderWhite(PrintStream out, String headerText) {
+        int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
+        int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
+
+        out.print(EMPTY.repeat(prefixLength));
+        printHeaderTextWhite(out, headerText);
+        out.print(EMPTY.repeat(suffixLength));
+    }
+
+    private static void printHeaderTextWhite(PrintStream out, String player) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_GREEN);
+
+        out.print(player);
+
+        setBlack(out);
+    }
+
+    private static void drawWhiteChessBoard(PrintStream out) {
+
+        for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
+            drawRowOfSquaresWhite(out, boardRow);
+        }
+    }
+
+    private static void drawRowOfSquaresWhite(PrintStream out, int boardRow) {
+        for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_PADDED_CHARS; ++squareRow) {
+
+            out.print(SET_BG_COLOR_BLACK);
+            out.print(SET_TEXT_COLOR_GREEN);
+            out.print(" " + (8 - boardRow) + " ");
+
+            for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+                if ((boardCol + boardRow) % 2 == 0) {
+                    setWhite(out);
+                } else {
+                    setLight(out);
+                }
+                printPlayer(out, INITIAL_BOARD_WHITE[boardRow][boardCol]);
+            }
+
+            out.print(SET_BG_COLOR_BLACK);
+            out.print(SET_TEXT_COLOR_GREEN);
+            out.print(" " + (8 - boardRow) + " ");
+            out.println();
+        }
+    }
+
+
+    /// BLACK BOARD SETUP
+    private static void drawHeadersBlack(PrintStream out) {
+        setBlack(out);
+
+        //offset for row #'s
+        out.print("   ");
+        String[] headers = { " h ", " g", " f ", " e ", " d ", " c ", " b ", " a " };
+        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+            drawHeaderBlack(out, headers[boardCol]);
+            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+                out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
+            }
+        }
+
+        setBlack(out);
+        out.print("  ");
+        out.println();
+    }
+
+    private static void drawFootersBlack(PrintStream out) {
+        setBlack(out);
+
+        // offset for row #'s
+        out.print("   ");
+        String[] footers = { " a ", " b ", " c ", " d ", " e ", " f ", " g ", " h " };
+        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+            drawHeaderBlack(out, footers[boardCol]);
+            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+                out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
+            }
+        }
+
+        setBlack(out);
+        out.print("  ");
+        out.println();
+    }
+
+    private static void drawHeaderBlack(PrintStream out, String headerText) {
+        int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
+        int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
+
+        out.print(EMPTY.repeat(prefixLength));
+        printHeaderTextBlack(out, headerText);
+        out.print(EMPTY.repeat(suffixLength));
+    }
+
+    private static void printHeaderTextBlack(PrintStream out, String player) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_GREEN);
+
+        out.print(player);
+
+        setBlack(out);
+    }
+
+    private static void drawBlackChessBoard(PrintStream out) {
+
+        for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
+            drawRowOfSquaresBlack(out, boardRow);
+        }
+    }
+
+    private static void drawRowOfSquaresBlack(PrintStream out, int boardRow) {
+        for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_PADDED_CHARS; ++squareRow) {
+
+            out.print(SET_BG_COLOR_BLACK);
+            out.print(SET_TEXT_COLOR_GREEN);
+            out.print(" " + (8 - boardRow) + " ");
+
+            for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+                if ((boardCol + boardRow) % 2 == 0) {
+                    setLight(out);
+                } else {
+                    setWhite(out);
+                }
+                printPlayer(out, INITIAL_BOARD_BLACK[boardRow][boardCol]);
+            }
+
+            out.print(SET_BG_COLOR_BLACK);
+            out.print(SET_TEXT_COLOR_GREEN);
+            out.print(" " + (8 - boardRow) + " ");
+            out.println();
+        }
+    }
+
+    private static void setWhite(PrintStream out) {
+        out.print(SET_BG_COLOR_WHITE);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void setRed(PrintStream out) {
+        out.print(SET_BG_COLOR_RED);
+        out.print(SET_TEXT_COLOR_RED);
+    }
+
+    private static void setBlack(PrintStream out) {
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_BLACK);
+    }
+
+    private static void setDark(PrintStream out) {
+        out.print(SET_BG_COLOR_DARK_GREY);
+        out.print(SET_TEXT_COLOR_DARK_GREY);
+    }
+
+    private static void setLight(PrintStream out) {
+        out.print(SET_BG_COLOR_LIGHT_GREY);
+        out.print(SET_TEXT_COLOR_LIGHT_GREY);
+    }
+
+    private static void setYellow(PrintStream out) {
+        out.print(SET_BG_COLOR_YELLOW);
+        out.print(SET_BG_COLOR_YELLOW);
+    }
+
+    private static void printPlayer(PrintStream out, String player) {
+        out.print(SET_TEXT_COLOR_BLACK);
+        out.print(player);
+    }
+}
