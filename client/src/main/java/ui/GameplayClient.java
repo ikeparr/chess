fa15ee5a06987653ed074531ec219312e9a7dba8
Isaac Parr <ikeparr@byu.edu>
2025-03-25 13:58:24 -0600

    implemented gameplay client similar to postlogin client. Auto prints board for right team color when join

--- client/src/main/java/ui/GameplayClient.java
+++ client/src/main/java/ui/GameplayClient.java
@@ -1,4 +1,68 @@
 package ui;
 
+import exceptions.ResponseException;
+import ui.ChessBoard;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 public class GameplayClient {
+    private final int serverUrl;
+    public String authToken;
+    public String color;
+
+    public GameplayClient(int serverUrl, String authToken, String color) {
+        this.serverUrl = serverUrl;
+        this.authToken = authToken;
+        this.color = color;
+    }
+
+    public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "leavegame" -> leaveGame();
+                case "move" -> move();
+                case "highlight" -> highlightMoves();
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
+
+    public void printBoard() {
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        if (color.equals("WHITE")) {
+            ChessBoard.drawWhiteBoard(out);
+        }
+        if (color.equals("BLACK")) {
+            ChessBoard.drawBlackBoard(out);
+        }
+    }
+
+    public String leaveGame() throws ResponseException {
+        return "User left the game\n";
+    }
+
+    public String move() throws ResponseException {
+        return null;
+    }
+
+    public String highlightMoves() throws ResponseException {
+        return null;
+    }
+
+    public String help() {
+        return """
+                - leaveGame
+                - move
+                - highlight
+                - help
+                """;
+    }
 }
+
