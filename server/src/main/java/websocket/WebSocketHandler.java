f522d98f5e6af582ec2c248e8b99aa87e573a9ce
Isaac Parr <ikeparr@byu.edu>
2025-04-10 11:01:25 -0600

    initial commit

---
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -0,0 +1,16 @@
+package websocket;
+
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
+import websocket.messages.*;
+
+
+import java.io.IOException;
+
+@WebSocket
+public class WebSocketHandler {
+
+
+
+}
