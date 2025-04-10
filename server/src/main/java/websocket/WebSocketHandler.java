199092b8aae29f5a9c067cbdb0e368339a7a1255
Isaac Parr <ikeparr@byu.edu>
2025-04-10 11:25:24 -0600

    added OnWebsocketMessage to WebSocketHandler, similar to petshop but adapted for chess

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -11,6 +11,21 @@ import java.io.IOException;
 @WebSocket
 public class WebSocketHandler {
 
+    private final ConnectionManager connections = new ConnectionManager();
+    private UserDAO userDAO = new SQLUser();
+    private GameDAO gameDAO = new SQLGame();
+    private AuthDAO authDAO = new SQLAuth();
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws IOException {
+        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        switch (command.getCommandType()) {
+            case CONNECT -> connect(command, session);
+            case MAKE_MOVE -> makeMove(command, session);
+            case LEAVE -> leave(command, session);
+            case RESIGN -> resign(command, session);
+        }
+    }
 
 
 }
