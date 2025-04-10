0f6c8b89693c1f88f23376638c09423947ca0e8e
Isaac Parr <ikeparr@byu.edu>
2025-04-10 11:16:39 -0600

    added ResponseException from client to use within server side code

---
+++ server/src/main/java/exceptions/ResponseException.java
@@ -0,0 +1,27 @@
+package exceptions;
+
+import com.google.gson.Gson;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseException extends Exception {
+    final private int statusCode;
+
+    public ResponseException(int statusCode, String message) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public String toJson() {
+        return new Gson().toJson(Map.of("message", getMessage(), "status", statusCode));
+    }
+
+    public static ResponseException fromJson(InputStream stream, int status) {
+        var map = new Gson().fromJson(new InputStreamReader(stream), HashMap.class);
+        String message = map.get("message").toString();
+        return new ResponseException(status, message);
+    }
+}
